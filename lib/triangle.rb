7c87a42d704f049647e7cb1ca98beffa8d4ac0a9
Jamal Rjoul <jamensky1991@gmail.com>
2020-01-05 21:51:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,5 @@
 class Triangle
-  attr_accessor :measure_1, :measure_2, :measure_3, :kind, :measures 
+  attr_accessor :measure_1, :measure_2, :measure_3, :kind, :measures
 
   @@all_triangles = []
 
@@ -8,47 +8,47 @@ class Triangle
       @measures << @measure_1 = measure_1
       @measures << @measure_2 = measure_2
       @measures << @measure_3 = measure_3
-      @@all_triangles << self 
+      @@all_triangles << self
   end
-  
+
   def self.all_triangles
     @@all_triangles
-  end  
+  end
 
   def multiples
     multiples = []
-    self.measures.each{|measure| multiples << self.measures.count(measure)} 
+    self.measures.each{|measure| multiples << self.measures.count(measure)}
     multiples
-  end 
+  end
 
   def valid
-    if self.measures.all?{|measure| measure == 0} || self.measures.any?{|measure| measure < 0} 
+    if self.measures.all?{|measure| measure == 0} || self.measures.any?{|measure| measure < 0}
       raise TriangleError
     end
-  end 
+  end
 
 
-  def inequality 
+  def inequality
     x = self.measures.min(2)
-    y = self.measures.max 
+    y = self.measures.max
     z = x[0] + x[1]
     y > z
-  end  
-  
+  end
+
 
   def kind
-    if self.valid || self.inequality  
+    if self.valid || self.inequality
       raise TriangleError
-    else  
+    else
       if multiples.include?(3)
        self.kind = :equilateral
       elsif multiples.include?(2)
        self.kind = :isosceles
       elsif multiples.include?(1)
        self.kind = :scalene
-      end 
-    end  
-  end 
+      end
+    end
+  end
 
   class TriangleError < StandardError
   end

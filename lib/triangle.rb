3ec47e1f5f4e8288f6caafdae09a2616c09904bb
Jamal Rjoul <jamensky1991@gmail.com>
2020-01-05 21:51:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,32 +1,54 @@
 class Triangle
-  attr_accessor :measure_1, :measure_2, :measure_3, :kind
+  attr_accessor :measure_1, :measure_2, :measure_3, :kind, :measures 
+
+  @@all_triangles = []
 
   def initialize(measure_1, measure_2, measure_3)
-     @measure_1 = measure_1
-     @measure_2 = measure_2
-     @measure_3 = measure_3
+      @measures = []
+      @measures << @measure_1 = measure_1
+      @measures << @measure_2 = measure_2
+      @measures << @measure_3 = measure_3
+      @@all_triangles << self 
   end
+  
+  def self.all_triangles
+    @@all_triangles
+  end  
 
-  def kind
+  def multiples
     multiples = []
-    all_measures = [@measure_1, @measure_2, @measure_3]
-    all_measures.each{|measure| multiples << all_measures.count(measure)}
+    self.measures.each{|measure| multiples << self.measures.count(measure)} 
+    multiples
+  end 
 
-     if all_measures.all?{|measure| measure == 0} || all_measures.any?{|measure| measure < 0}
-      raise TriangleError
-    else
-      if multiples.include?(3)
-        self.kind = :equilateral
-      elsif multiples.include?(2)
-        self.kind = :isosceles
-      elsif multiples.include?(1)
-        self.kind = :scalene
-      end
-
-    else
+  def valid
+    if self.measures.all?{|measure| measure == 0} || self.measures.any?{|measure| measure < 0} 
       raise TriangleError
     end
-  end
+  end 
+
+
+  def inequality 
+    x = self.measures.min(2)
+    y = self.measures.max 
+    z = x[0] + x[1]
+    y > z
+  end  
+  
+
+  def kind
+    if self.valid || self.inequality  
+      raise TriangleError
+    else  
+      if multiples.include?(3)
+       self.kind = :equilateral
+      elsif multiples.include?(2)
+       self.kind = :isosceles
+      elsif multiples.include?(1)
+       self.kind = :scalene
+      end 
+    end  
+  end 
 
   class TriangleError < StandardError
   end

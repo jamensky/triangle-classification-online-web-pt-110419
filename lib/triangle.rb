af1ef9adacb5bfdf32722b0956e91274d930e3c5
Jamal Rjoul <jamensky1991@gmail.com>
2020-01-05 19:25:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_accessor :measure_1, :measure_2, :measure_3, :kind
+
+  def initialize(measure_1, measure_2, measure_3)
+     @measure_1 = measure_1
+     @measure_2 = measure_2
+     @measure_3 = measure_3
+  end
+
+  def kind
+    multiples = []
+    all_measures = [@measure_1, @measure_2, @measure_3]
+    all_measures.each{|measure| multiples << all_measures.count(measure)}
+
+     if all_measures.all?{|measure| measure == 0} || all_measures.any?{|measure| measure < 0}
+      raise TriangleError
+    else
+      if multiples.include?(3)
+        self.kind = :equilateral
+      elsif multiples.include?(2)
+        self.kind = :isosceles
+      elsif multiples.include?(1)
+        self.kind = :scalene
+      end
+
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
